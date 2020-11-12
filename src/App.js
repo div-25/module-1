5ce06e6b0375d4b9148115966824c04cf0d28dd5
Divyansh Hardia <divyanshhardia10@gmail.com>
2020-11-13 02:35:58 +0530

    Updated project

--- src/App.js
+++ src/App.js
@@ -1,85 +1,15 @@
 import React, { Component } from "react";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import injectTapEventPlugin from "react-tap-event-plugin";
-
 import logo from "./logo.svg";
 import "./App.css";
-import Form from "./Form";
-import Table from "./Table";
 
-injectTapEventPlugin();
+import Main from './components/MainComponent';
 
 class App extends Component {
-  state = {
-    data: [],
-    editIdx: -1
-  };
-
-  handleRemove = i => {
-    this.setState(state => ({
-      data: state.data.filter((row, j) => j !== i)
-    }));
-  };
-
-  startEditing = i => {
-    this.setState({ editIdx: i });
-  };
-
-  stopEditing = () => {
-    this.setState({ editIdx: -1 });
-  };
-
-  handleChange = (e, name, i) => {
-    const { value } = e.target;
-    this.setState(state => ({
-      data: state.data.map(
-        (row, j) => (j === i ? { ...row, [name]: value } : row)
-      )
-    }));
-  };
-
   render() {
     return (
-      <MuiThemeProvider>
-        <div className="App">
-          <Form
-            onSubmit={submission =>
-              this.setState({
-                data: [...this.state.data, submission]
-              })}
-          />
-          <Table
-            handleRemove={this.handleRemove}
-            startEditing={this.startEditing}
-            editIdx={this.state.editIdx}
-            stopEditing={this.stopEditing}
-            handleChange={this.handleChange}
-            data={this.state.data}
-            header={[
-              {
-                name: "Effective From",
-                prop: "effectiveFrom"
-              },
-              {
-                name: "Course",
-                prop: "course"
-              },
-              {
-                name: "Minimum Credit Point",
-                prop: "minimumCreditPoint"
-              },
-              {
-                name: "Maximum Credit Point",
-                prop: "maximumCreditPoint"
-              },
-              {
-                name: "Remarks",
-                prop: "remarks"
-              }
-            ]}
-          />
-        </div>
-      </MuiThemeProvider>
+      <div className="App">
+        <Main />
+      </div>
     );
   }
 }
